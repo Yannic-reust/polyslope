1928f634fb21e0ec8dd35447f847deb8e5048990
Ramon <ramongrutschnig@gmail.com>
2023-11-03 11:31:18 +0100

    feat: added sidebar component

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+const SideBar = () => {
+    return ( 
+        <nav className="fixed right-0 top-0 h-screen w-24 bg-white bg-opacity-20 z-[1035]">
+            <h3>Link 1</h3>
+            <h3>Link 2</h3>
+        </nav>
+     );
+}
+ 
+export default SideBar;

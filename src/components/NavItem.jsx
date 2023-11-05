ff6432f3743eaf1cd6f303fd49606da21474823a
Ramon <ramongrutschnig@gmail.com>
2023-11-05 15:56:24 +0100

    fix: removed color change on icon hover

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -7,10 +7,10 @@ const NavItem = (props) => {
 
     return (
         <div onClick={ () => props.togglePage( props.title ) } className="flex flex-col items-center icon group cursor-pointer">
-            <div className={`group-hover:fill-accent transition-colors duration-300 ${ isTabActive() ? "fill-accent" : "fill-white" }`}>
+            <div className={`transition-colors duration-300 ${ isTabActive() ? "fill-accent" : "fill-white" }`}>
                 { props.icon }
             </div>
-            <span className={`text-label text-center mt-1 transition-all duration-300 group-hover:opacity-100 group-hover:text-accent ${ isTabActive() ? "opacity-100 text-accent" : "opacity-0" }`}>{ props.title }</span>
+            <span className={`text-label text-center mt-1 transition-all duration-300 group-hover:opacity-100 ${ isTabActive() ? "opacity-100 text-accent" : "opacity-0" }`}>{ props.title }</span>
         </div>
      );
 }

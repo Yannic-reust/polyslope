289f9534aa00ce4659ad9d319f84aa349ca0eb5e
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-11-06 21:55:41 +0100

    fix: changed font & fixed props validation

--- src/components/ContentIntroduction/ContentIntroduction.jsx
+++ src/components/ContentIntroduction/ContentIntroduction.jsx
@@ -1,14 +1,20 @@
 import "./ContentIntroduction.css";
+import PropTypes from "prop-types";
 
 const ContentIntroduction = ({ title, text }) => {
   return (
     <div>
-      <div className="mb-8 font-karmina">
-        <h2 className="text-h-lg">{title}</h2>
+      <div className="mb-8">
+        <h2 className="text-h-lg font-karmina">{title}</h2>
         <p>{text}</p>
       </div>
     </div>
   );
+};
+
+ContentIntroduction.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
 };
 
 export default ContentIntroduction;

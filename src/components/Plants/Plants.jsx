8ea960e778c7f8d3e403ade34dfb37e2ba331fd3
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-11-08 14:35:45 +0100

    feat: change icon on state change accordion

--- src/components/Plants/Plants.jsx
+++ src/components/Plants/Plants.jsx
@@ -50,7 +50,7 @@ function Plants() {
         <div>
           {items.map((item, index) => (
             <div key={index}>
-              <ContentSection item={item} icon={item.icon} />
+              <ContentSection item={item} icon={item.icon} camera={false}/>
             </div>
           ))}
         </div>

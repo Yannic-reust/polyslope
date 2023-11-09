be9380839e6d287b7a99db29a48864b125b970ae
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-11-09 14:47:36 +0100

    feat: wip install redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import SideBar from "./components/SideBar/SideBar";
 import TabBar from "./components/TabBar/TabBar";
 import Experience from "./components/Experience";
+import StoreDemo from "./components/StoreDemo";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <SideBar />
       <TabBar />
+      <StoreDemo />
       <Experience />
     </>
   );
---
+++ src/components/StoreDemo.jsx
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+
+
+const StoreDemo = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
+export default StoreDemo;

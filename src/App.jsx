c0226e766f91e18471e05d6a20f42ad90f3bdbfc
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-11-10 09:34:59 +0100

    feat: added cursor hover and element placement

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import "./App.css";
 
 import SideBar from "./components/SideBar/SideBar";
 import TabBar from "./components/TabBar/TabBar";
-import Experience from "./components/Experience";
-import StoreDemo from "./components/StoreDemo";
-
+import AnimationsBadge from "./components/AnimationsBadge/AnimationsBadge";
+import AnimationsStatus from "./components/AnimationsStatus/AnimationsStatus";
 
 function App() {
   return (
     <>
-      <SideBar />
-      <TabBar />
-      <StoreDemo />
-      {/* <Experience /> */}
+      <div className="relative h-screen ">
+        <SideBar />
+        <TabBar />
+
+        <div className="absolute bottom-8  w-full flex items-center">
+          <div className="ml-8">
+            <AnimationsBadge />
+          </div>
+          <div className=" absolute left-1/2 transform -translate-x-1/2 ">
+            <AnimationsStatus />
+          </div>
+        </div>
+
+        {/* <Experience /> */}
+      </div>
     </>
   );
 }
---
+++ src/components/AnimationsBadge/AnimationsBadge.jsx
@@ -0,0 +1,22 @@
+import { useSelector, useDispatch } from "react-redux";
+import { toggleAnimation } from "../../store/animations/animationsState";
+import AnimationsOnSVG from "../../assets/icons/animations.svg?react";
+
+
+const AnimationsBadge = () => {
+  const count = useSelector((state) => state.animation.value);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div
+        className="w-20 h-20 rounded-full bg-darkblue/80  flex  justify-center items-center p-4 cursor-pointer"
+        onClick={() => dispatch(toggleAnimation())}
+      >
+        <AnimationsOnSVG className="w-8 " />
+      </div>
+    
+    </>
+  );
+};
+export default AnimationsBadge;

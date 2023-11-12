5ec8f6410c11cbe8f0ee3f01a57a1f14fd6d6a46
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-11-12 14:01:26 +0100

    feat: temperatures working

---
+++ src/components/WeatherTile/useWeatherData.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+//const [data, setData] = useState(null);
+
+const useWeatherData = (props) => {
+  //console.log(props);
+  const [tempLow, setLowest] = useState(null);
+  const [tempHigh, setHighest] = useState(null);
+
+  function roundNumbers(arr) {
+    return arr.map((number) => {
+      if (number >= 0.5) {
+        return Math.ceil(number);
+      } else {
+        return Math.floor(number);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (props && props.length === 24) {
+      const lowest = Math.min(...roundNumbers(props));
+      const highest = Math.max(...roundNumbers(props));
+      setLowest(lowest);
+      setHighest(highest);
+    }
+  }, [props]);
+
+  return { tempLow, tempHigh };
+};
+
+export default useWeatherData;

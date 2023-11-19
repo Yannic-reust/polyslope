ff4335d88a7d99bb65245382018cfdb993ba77ef
Cyril Hügli <cyril.huegli@gmail.com>
2023-11-19 18:35:55 +0100

    canvas implementation (no scaling)

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -18,15 +18,14 @@ const Canvas = () => {
         <div className="absolute canvas-container h-screen w-screen bg-darkblue z-[-1]">
             <R3fCanvas 
                 camera={{
-                    near: 5, // stops flickering when viewed from distance
-                    far: 50000,
-                    position: [5500, 6000, 0],
+                    far: 50000, 
+                    position: [6000, 2000, -6000],
                     minDistance: 3000,
                     maxDistance: 300000
                 }}
             >
                 <Light distance={8000}/>
-                <Detailed distances={[0, 450, 600]}>
+                <Detailed distances={[0, 4500, 6000]}>
                     <LandscapeHigh />
                     <LandscapeMed />
                     <LandscapeLow />

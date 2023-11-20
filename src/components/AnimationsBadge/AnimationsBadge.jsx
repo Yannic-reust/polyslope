75f70d6fc1ad53be4920db6f36bc7fe79790cfda
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-11-20 14:18:56 +0100

    feat: overlay on about

--- src/components/AnimationsBadge/AnimationsBadge.jsx
+++ src/components/AnimationsBadge/AnimationsBadge.jsx
@@ -1,18 +1,16 @@
-import { useDispatch } from "react-redux";
+
 import { useSelector } from "react-redux";
-import { toggleAnimation } from "../../store/animations/animationsState";
 import AnimationsOnSVG from "../../assets/icons/animations.svg?react";
 import AnimationsOffSVG from "../../assets/icons/animation-off.svg?react";
 
 const AnimationsBadge = () => {
   const active = useSelector((state) => state.animation.value);
-  const dispatch = useDispatch();
+ 
 
   return (
     <>
       <div
         className="w-20 h-20 rounded-full bg-darkblue/80  flex  justify-center items-center p-4 cursor-pointer"
-        onClick={() => dispatch(toggleAnimation())}
       >
         {active && <AnimationsOnSVG className="w-8 " />}
         {!active && <AnimationsOffSVG className="w-8 " />}

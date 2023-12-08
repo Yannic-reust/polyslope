979a528013f27aacba81dd63fea112f0a998fc37
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-12-08 23:28:49 +0100

    feat: slopes badge

---
+++ src/components/ToolTips/BadgeSlopes.jsx
@@ -0,0 +1,21 @@
+import "./BadgeSlopes.css";
+import PropTypes from "prop-types";
+
+function BadgeSlopes({ BadgeNumber, BageColor }) {
+  return (
+    <>
+      <div className={`circle bg-[${BageColor}]`}   style={{backgroundColor: `${BageColor}`}}>
+        <p className="circle__content text-white  ">{BadgeNumber}</p>
+      </div>
+    </>
+  );
+}
+
+BadgeSlopes.propTypes = {
+  BadgeNumber: PropTypes.string.isRequired,
+  BageColor: PropTypes.string.isRequired,
+
+};
+
+
+export default BadgeSlopes;

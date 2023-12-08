6905386d76065fea3a92c00bbb0ad7a15e417a9d
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-12-08 16:55:58 +0100

    feat: added track data

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import animationsReducer from './animations/animationsState'
 import restaurantReducer from './restaurant/restaurantState'
 import liftReducer from './lift/liftState'
+import trackReducer from './track/trackState'
 
 export default configureStore({
   reducer: {
     animation: animationsReducer,
     restaurant: restaurantReducer,
     lift: liftReducer,
+    track: trackReducer,
   },
 })

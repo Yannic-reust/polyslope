47cb118281b5128607bef9429030cf990ae92786
Ramon <ramongrutschnig@gmail.com>
2023-12-14 12:39:54 +0100

    feat: fixed tooltips and slope settings

--- src/components/ToolTips/ToolTipLifts.jsx
+++ src/components/ToolTips/ToolTipLifts.jsx
@@ -40,7 +40,7 @@ function ToolTipLifts({ handle, lift }) {
       <div className={`contentLift select-none ${open ? "lineLift" : ""}`} onClick={toggleOpen}>
         <div className="flex">
           {!open && getPlantIcon(lift.type ?? '')}
-          {open && <p className="text-p-md font-semibold text-center text-white">{LiftName}</p>}
+          {open && <p className="text-p-md font-semibold text-center text-white">{lift.name}</p>}
         </div>
         {open && (
           <div>

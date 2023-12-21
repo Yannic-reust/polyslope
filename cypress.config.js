875e35bfcba9f40ecdee28b0be9a9b0922018c2e
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-12-21 18:57:08 +0100

    feat: add report funciton to cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "cypress";
-
+import cypressMochawesomeReporterPlugin from 'cypress-mochawesome-reporter/plugin.js';
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      cypressMochawesomeReporterPlugin(on);
       // implement node event listeners here
     },
   },

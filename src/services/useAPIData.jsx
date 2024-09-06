73015172aaf37009e44a20b9a53f498230e8dac2
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-09-06 15:08:10 +0200

    chore: point to backup

--- src/services/useAPIData.jsx
+++ src/services/useAPIData.jsx
@@ -11,17 +11,6 @@ const useAPIData = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://www.jungfrau.ch/api/resort/v01/winter/overview"
-        );
-        const newData = await response.json();
-
-        dispatch(setRestaurants(newData.data.resorts[2].restaurants));
-        dispatch(setLift(newData.data.resorts[2].lifts.slice(0, 19)));
-        dispatch(setTrack(newData.data.resorts[2].tracks));
-        dispatch(setSnowData(newData.data.resorts[2].summary));
-
-      } catch (error) {
         const existingDataResponse = await fetch(
           "../../public/backup_data.json"
         );
@@ -31,7 +20,7 @@ const useAPIData = () => {
         dispatch(setLift(existingData.lifts.slice(0, 19)));
         dispatch(setTrack(existingData.tracks));
         dispatch(setSnowData(existingData.summary));
-
+      } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
@@ -41,3 +30,35 @@ const useAPIData = () => {
 };
 
 export default useAPIData;
+
+/**** old api fetch ****/
+/*useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://www.jungfrau.ch/api/resort/v01/winter/overview"
+      );
+      const newData = await response.json();
+
+      dispatch(setRestaurants(newData.data.resorts[2].restaurants));
+      dispatch(setLift(newData.data.resorts[2].lifts.slice(0, 19)));
+      dispatch(setTrack(newData.data.resorts[2].tracks));
+      dispatch(setSnowData(newData.data.resorts[2].summary));
+
+    } catch (error) {
+      const existingDataResponse = await fetch(
+        "../../public/backup_data.json"
+      );
+      const existingData = await existingDataResponse.json();
+
+      dispatch(setRestaurants(existingData.restaurants));
+      dispatch(setLift(existingData.lifts.slice(0, 19)));
+      dispatch(setTrack(existingData.tracks));
+      dispatch(setSnowData(existingData.summary));
+
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  fetchData();
+}, [dispatch]);*/
